34b6c39e9a29c4f1729dafd0a995ac2e4a71c9bb
Kirubel K <joejnke@gmail.com>
2019-10-08 13:00:42 +0300

    untested fixes to isFull methods and exception handles

--- Percolation.java
+++ Percolation.java
@@ -1,13 +1,11 @@
-/* *****************************************************************************
+/******************************************************************************
  *  Name:
  *  Date:
  *  Description:
  **************************************************************************** */
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import java.util.logging.Logger;
 
 public class Percolation {
-    final Logger logger = Logger.getLogger(Percolation.class.getName());
     final WeightedQuickUnionUF siteGrid;
     private int numOpenSites;
     private boolean[] isOpenArray;
@@ -16,12 +14,7 @@ public class Percolation {
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n <= 0) {
-            try {
-                throw new IllegalArgumentException("value given for n is Out of range...");
-            }
-            catch (IllegalArgumentException e) {
-                logger.warning(e.toString());
-            }
+            throw new IllegalArgumentException("n <= 0 is not allowed...");
         }
         gridSize = n;
         siteGrid = new WeightedQuickUnionUF(gridSize * gridSize);
@@ -38,30 +31,34 @@ public class Percolation {
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         if ((row-1)*(this.gridSize - row) < 0 || (col-1)*(this.gridSize - col) < 0) {
-            try {
-                throw new IllegalArgumentException("value given for (row, col) is Out of range...");
-            }
-            catch (IllegalArgumentException e) {
-                logger.warning(e.toString());
-            }
+            throw new IllegalArgumentException("value given for (row, col) is Out of range...");
         }
-        if (!isOpen(row, col)) {
-            this.isOpenArray[this.indexFrom2d(row, col)] = true;
+        else if (!isOpen(row, col)) {
+            this.isOpenArray[this.indexFrom2d(row, col)] = true;    // open the site.
 
-            if (!(row == 1) && isOpen(row-1, col)) {
-                siteGrid.union(this.indexFrom2d(row, col), this.indexFrom2d(row-1, col));
+            // create connection with neighbouring open sites.
+            if (!(row == 1)) {
+                if (isOpen(row-1, col)) {
+                    siteGrid.union(this.indexFrom2d(row, col), this.indexFrom2d(row-1, col));
+                }
             }
 
-            if (!(row == this.gridSize) && isOpen(row+1, col)) {
-                siteGrid.union(this.indexFrom2d(row, col), this.indexFrom2d(row+1, col));
+            if (!(row == this.gridSize)) {
+                if (isOpen(row+1, col)) {
+                    siteGrid.union(this.indexFrom2d(row, col), this.indexFrom2d(row+1, col));
+                }
             }
 
-            if (!(col == 1) && isOpen(row, col-1)) {
-                siteGrid.union(this.indexFrom2d(row, col), this.indexFrom2d(row, col-1));
+            if (!(col == 1)) {
+                if (isOpen(row, col-1)) {
+                    siteGrid.union(this.indexFrom2d(row, col), this.indexFrom2d(row, col-1));
+                }
             }
 
-            if (!(col == this.gridSize) && isOpen(row, col+1)) {
-                siteGrid.union(this.indexFrom2d(row, col), this.indexFrom2d(row, col+1));
+            if (!(col == this.gridSize)) {
+                if (isOpen(row, col+1)) {
+                    siteGrid.union(this.indexFrom2d(row, col), this.indexFrom2d(row, col+1));
+                }
             }
 
             this.numOpenSites++;
@@ -71,12 +68,7 @@ public class Percolation {
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         if ((row - 1)*(this.gridSize - row) < 0 || (col - 1)*(this.gridSize - col) < 0) {
-            try {
-                throw new IllegalArgumentException("value given for (row, col) is Out of range...");
-            }
-            catch (IllegalArgumentException e) {
-                logger.warning(e.toString());
-            }
+            throw new IllegalArgumentException("value given for (row, col) is Out of range...");
         }
         return this.isOpenArray[this.indexFrom2d(row, col)];
     }
@@ -84,55 +76,62 @@ public class Percolation {
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         if ((row-1)*(this.gridSize - row) < 0 || (col-1)*(this.gridSize - col) < 0) {
-            try {
-                throw new IllegalArgumentException("value given for (row, col) is Out of range...");
-            }
-            catch (IllegalArgumentException e) {
-                logger.warning(e.toString());
-            }
+            throw new IllegalArgumentException("value given for (row, col) is Out of range...");
         }
         int p = this.indexFrom2d(row, col);
 
-        if (!this.isFullArray[p]) {
-            for (int i = 0; i < this.gridSize; i++) {
-                if (this.siteGrid.connected(p, i)) {
-                    this.isFullArray[p] = true;
-                    this.isOpenArray[p] = true;
+        // if (!this.isFullArray[p]) {
+        //     for (int i = 0; i < this.gridSize; i++) {
+        //         if (this.siteGrid.connected(p, i)) {
+        //             this.isFullArray[p] = true;
+        //             this.isOpenArray[p] = true;
+        //
+        //             return this.isFullArray[p];
+        //         }
+        //     }
+        //
+        //     return this.isFullArray[p];
+        // }
+        //
+        // return this.isFullArray[p];
 
-                    return true;
-                }
+        for (int i = 0; i < this.gridSize; i++) {
+            if (this.siteGrid.connected(p, i)) {
+                return true;
             }
-
-            return false;
         }
 
-        return true;
+        return false;
     }
 
-    // is the site p full? where p is etween 0 and n*n
+    // is the site p full? where p is between 0 and n*n
     private boolean isFull(int p) {
         if ((p)*(this.gridSize *this.gridSize -1 - p) < 0) {
-            try {
-                throw new IllegalArgumentException("value given for p is Out of range...");
-            }
-            catch (IllegalArgumentException e) {
-                logger.warning(e.toString());
+            throw new IllegalArgumentException("value given for p is Out of range...");
+        }
+
+        // if (!this.isFullArray[p]) {
+        //     for (int i = 0; i < this.gridSize; i++) {
+        //         if (this.siteGrid.connected(p, i)) {
+        //             this.isFullArray[p] = true;
+        //             this.isOpenArray[p] = true;
+        //
+        //             return this.isFullArray[p];
+        //         }
+        //     }
+        //
+        //     return this.isFullArray[p];
+        // }
+        //
+        // return this.isFullArray[p];
+
+        for (int i = 0; i < this.gridSize; i++) {
+            if (this.siteGrid.connected(p, i)) {
+                return true;
             }
         }
 
-        if (!this.isFullArray[p]) {
-            for (int i = 0; i < this.gridSize; i++) {
-                if (this.siteGrid.connected(p, i)) {
-                    this.isFullArray[p] = true;
-                    this.isOpenArray[p] = true;
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        return true;
+        return false;
     }
 
     // returns the number of open sites
--- PercolationStats.java
+++ PercolationStats.java
@@ -4,9 +4,9 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.StdOut;
 
 public class PercolationStats {
     static final double CONFIDENCE_95 = 1.96E00;
@@ -16,18 +16,28 @@ public class PercolationStats {
     private int t;
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException("values of n <= 0 and trials <= 0 are not allowed...");
+        }
+
         this.t = trials;
         this.pThreshArray = new double[t];
         for (int i = 0; i < t; i++) {
-            Percolation perc = new Percolation(n);
+            try {
+                Percolation perc = new Percolation(n);
 
-            while (!(perc.percolates())) { //|| Perc.numberOfOpenSites() == n*n)){
-                int row = StdRandom.uniform(1, n+1); // random integers in the range [1, n]
-                int col = StdRandom.uniform(1, n+1); // random integers in the range [1, n]
-                perc.open(row, col);
+                while (!(perc.percolates())) { //|| Perc.numberOfOpenSites() == n*n)){
+                    int row = StdRandom.uniform(1, n+1); // random integers in the range [1, n]
+                    int col = StdRandom.uniform(1, n+1); // random integers in the range [1, n]
+                    perc.open(row, col);
+                }
+
+                this.pThreshArray[i] = ((double) perc.numberOfOpenSites()) / (n*n);
             }
 
-            this.pThreshArray[i] = ((double) perc.numberOfOpenSites()) / (n*n);
+            catch (IllegalArgumentException e) {
+                System.out.println(e.toString());
+            }
         }
     }
 
@@ -57,10 +67,19 @@ public class PercolationStats {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int trials = Integer.parseInt(args[1]);
-        PercolationStats percStat = new PercolationStats(n, trials);
-        StdOut.printf("%-23s = %f\n", "mean", percStat.mean());
-        StdOut.printf("%-23s = %f\n", "stddev", percStat.stddev());
-        StdOut.printf("%-23s = [%f, %f]\n", "95% confidence interval", percStat.confidenceLo(), percStat.confidenceHi());
+
+        try {
+            PercolationStats percStat = new PercolationStats(n, trials);
+            StdOut.printf("%-23s = %f\n", "mean", percStat.mean());
+            StdOut.printf("%-23s = %f\n", "stddev", percStat.stddev());
+            StdOut.printf("%-23s = [%f, %f]\n", "95% confidence interval", percStat.confidenceLo(),
+                          percStat.confidenceHi());
+        }
+
+        catch (IllegalArgumentException e) {
+            System.out.println(e.toString());
+        }
+
     }
 
 }
