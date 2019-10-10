792fe6413161c3bf27dd15a9648cdcc49d95e4d7
Kirubel K <joejnke@gmail.com>
2019-10-10 17:50:50 +0300

    clean up code

--- Percolation.java
+++ Percolation.java
@@ -26,10 +26,6 @@ public class Percolation {
         this.isOpenArray = new boolean[gridSize * gridSize];
         this.isFullArray = new boolean[gridSize * gridSize];
         this.isBottomArray = new boolean[gridSize * gridSize];
-        // this.compIdArr = new int[gridSize * gridSize];
-        // for (int i = 0; i < this.gridSize; i++) {
-        //     this.compIdArr[i] = i;
-        // }
     }
 
     // convert a given (row,col) pair into an index of [0, n*n-1] where both row and col are integers between 1 and n;
@@ -69,8 +65,6 @@ public class Percolation {
                     }
 
                     siteGrid.union(this.indexFrom2d(row, col), this.indexFrom2d(row - 1, col));
-                    // this.compIdArr[this.indexFrom2d(row, col)] = siteGrid.find(this.indexFrom2d(row, col));
-                    // this.compIdArr[this.indexFrom2d(row - 1, col)] = this.compIdArr[this.indexFrom2d(row, col)];
                 }
             }
 
@@ -88,8 +82,6 @@ public class Percolation {
                     }
 
                     siteGrid.union(this.indexFrom2d(row, col), this.indexFrom2d(row + 1, col));
-                    // this.compIdArr[this.indexFrom2d(row, col)] = siteGrid.find(this.indexFrom2d(row, col));
-                    // this.compIdArr[this.indexFrom2d(row + 1, col)] = this.compIdArr[this.indexFrom2d(row, col)];
                 }
             }
 
@@ -106,8 +98,6 @@ public class Percolation {
                         this.isBottomArray[siteGrid.find(this.indexFrom2d(row, col - 1))] = true;
                     }
                     siteGrid.union(this.indexFrom2d(row, col), this.indexFrom2d(row, col-1));
-                    // this.compIdArr[this.indexFrom2d(row, col)] = siteGrid.find(this.indexFrom2d(row, col));
-                    // this.compIdArr[this.indexFrom2d(row, col - 1)] = this.compIdArr[this.indexFrom2d(row, col)];
                 }
             }
 
@@ -125,8 +115,6 @@ public class Percolation {
                     }
 
                     siteGrid.union(this.indexFrom2d(row, col), this.indexFrom2d(row, col+1));
-                    // this.compIdArr[this.indexFrom2d(row, col)] = siteGrid.find(this.indexFrom2d(row, col));
-                    // this.compIdArr[this.indexFrom2d(row, col + 1)] = this.compIdArr[this.indexFrom2d(row, col)];
                 }
             }
 
@@ -160,37 +148,6 @@ public class Percolation {
         }
 
         return this.isFullArray[this.siteGrid.find(this.indexFrom2d(row, col))];
-
-        // int p = this.indexFrom2d(row, col);
-        // if (!this.isFullArray[p]) {
-        //     for (int i = 0; i < this.gridSize; i++) {
-        //         if (this.siteGrid.connected(p, i)) {
-        //             this.isFullArray[p] = true;
-        //             this.isOpenArray[p] = true;
-        //
-        //             return this.isFullArray[p];
-        //         }
-        //     }
-        //
-        //     return this.isFullArray[p];
-        // }
-        //
-        // return this.isFullArray[p];
-        //
-        // if (this.isFullArray[p]) {
-        //     return true;
-        // }
-        //
-        // else {
-        //     for (int i = 0; i < this.gridSize; i++) {
-        //         if (isOpen(1, i + 1) && this.siteGrid.connected(p, i)) {
-        //             this.isFullArray[p] = true;
-        //             return true;
-        //         }
-        //     }
-        // }
-
-        // return false;
     }
 
     // is the site p full? where p is between 0 and n*n
@@ -200,36 +157,6 @@ public class Percolation {
         }
 
         return this.isFullArray[this.siteGrid.find(p)];
-
-        // if (!this.isFullArray[p]) {
-        //     for (int i = 0; i < this.gridSize; i++) {
-        //         if (this.siteGrid.connected(p, i)) {
-        //             this.isFullArray[p] = true;
-        //             this.isOpenArray[p] = true;
-        //
-        //             return this.isFullArray[p];
-        //         }
-        //     }
-        //
-        //     return this.isFullArray[p];
-        // }
-        //
-        // return this.isFullArray[p];
-
-        // if (this.isFullArray[p]) {
-        //     return true;
-        // }
-        //
-        // else {
-        //     for (int i = 0; i < this.gridSize; i++) {
-        //         if (isOpen(1, i + 1) && this.siteGrid.connected(p, i)) {
-        //             this.isFullArray[p] = true;
-        //             return true;
-        //         }
-        //     }
-        // }
-        //
-        // return false;
     }
 
     // returns the number of open sites
@@ -238,44 +165,29 @@ public class Percolation {
     }
 
     // does the system percolate?
-    // determine if any one of the bottom sites is fully connected.
     public boolean percolates() {
         return this.isPercolated;
-        // int maxIndex = this.gridSize *this.gridSize;
-        // for (int i = maxIndex - this.gridSize; i < maxIndex; i++) {
-        //     if (this.isOpen(i) && this.isFull(i)) {
-        //         return true;
-        //     }
-        // }
-        // return false;
     }
 
-    // public static void main(String[] args) {
-    //     int n = Integer.parseInt(args[0]);
-    //     // int trials = Integer.parseInt(args[1]);
-    //     int row = -2147483648;
-    //     int col = -2147483648;
-    //
-    //     try {
-    //         Percolation perc = new Percolation(n);
-    //         System.out.println(perc.isFull(row, col));
-    //
-    //         System.out.println("isFull isn't working fine");
-    //         System.out.println(perc.gridSize);
-    //         System.out.println((row-1)*(perc.gridSize - row));
-    //         System.out.println((col-1)*(perc.gridSize - col));
-    //         System.out.println((row-1)*(perc.gridSize - row) < 0 || (col-1)*(perc.gridSize - col) < 0);
-    //
-    //         // StdOut.printf("%-23s = %f\n", "mean", percStat.mean());
-    //         // StdOut.printf("%-23s = %f\n", "stddev", percStat.stddev());
-    //         // StdOut.printf("%-23s = [%f, %f]\n", "95% confidence interval", percStat.confidenceLo(),
-    //         //               percStat.confidenceHi());
-    //     }
-    //
-    //     catch (IllegalArgumentException e) {
-    //         System.out.println(e.toString());
-    //         System.out.println("isFull is working fine");
-    //     }
-    //
-    // }
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        // int trials = Integer.parseInt(args[1]);
+        int row = -2147483648;
+        int col = -2147483648;
+
+        try {
+            Percolation perc = new Percolation(n);
+            System.out.println(perc.isFull(row, col));
+
+            System.out.println("isFull isn't working fine");
+            System.out.println(perc.gridSize);
+
+        }
+
+        catch (IllegalArgumentException e) {
+            System.out.println(e.toString());
+            System.out.println("isFull is working fine");
+        }
+
+    }
 }
