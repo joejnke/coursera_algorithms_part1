05403d9f958568207fc7cbcc24a2aa568204433b
Kirubel K <joejnke@gmail.com>
2019-10-18 03:49:20 +0300

    FastCollinearPoints implemented with run time and memory spec unconsidered

--- week3_programming_assignment_collinear/collinear/FastCollinearPoints.java
+++ week3_programming_assignment_collinear/collinear/FastCollinearPoints.java
@@ -82,69 +82,156 @@ public class FastCollinearPoints {
             System.out.println("out######### " + p.toString() + " ##########");
         }
 
-        if (points.length >= 4) {
-            for (Point p0 : points) {
-                Point[] tempPoints = points.clone();
-                Arrays.sort(tempPoints, p0.slopeOrder());
-                System.out.println("######### ######## ##########");
-                for (Point p : tempPoints) {
-                    System.out.println("######### " + p.toString() + " ##########");
-                }
-                System.out.println("######### ######## ##########");
+        for (Point centerPoint : points) {
+            Point[] tempPoints = points.clone();
 
-                System.out.println("######### " + p0.toString() + " ##########");
-                Point lineStart = tempPoints[0];
-                Point lineEnd = tempPoints[1];
-                double refSlope = p0.slopeTo(lineEnd);
-                this.numPoints = 2;
+            // sort with slope order relative to centerPoint
+            Arrays.sort(tempPoints, centerPoint.slopeOrder());
 
-                for (int i = 1; i < tempPoints.length; i++) {
-                    if (Double.compare(p0.slopeTo(tempPoints[i]), refSlope) == 0) {
-                        if (tempPoints[i].compareTo(lineEnd) > 0) {
-                            // set prevEnd point to start if its less than both current start and end points
-                            if (lineEnd.compareTo(lineStart) < 0) { lineStart = lineEnd; }
+            /* assuming there are atleast four points in the point list */
+            Point[] tempLineStartEnd = new Point[]{centerPoint, tempPoints[1]};
+            // after sorted, tempLineStartEnd[0] will be start of the line segment and tempLineStartEnd[1] will be end of the line segment
+            Arrays.sort(tempLineStartEnd);
 
-                            // update current end point
-                            lineEnd = tempPoints[i];
-                        }
+            Point lineStart = tempLineStartEnd[0]; // set to minimum of centerPoint and tempPoints[1]
+            Point lineEnd = tempLineStartEnd[1]; // set to maximum of centerPoint and tempPoints[1]
+            double refSlope = centerPoint.slopeTo(tempPoints[1]);
+            int numCollPoints = 2;
 
-                        if (tempPoints[i].compareTo(lineStart) < 0) {
-                            // set prevStart point to end if its greater than both current start and end points
-                            if (lineStart.compareTo(lineEnd) > 0) { lineEnd = lineStart; }
-
-                            // update current start point
-                            lineStart = tempPoints[i];
-                        }
-
-                        this.numPoints++;
-                    }
-
-                    else {
-                        refSlope = p0.slopeTo(tempPoints[i]);
-                        if (this.numPoints > 3) {
-                            LineSegment coll = new LineSegment(lineStart, lineEnd);
-                            if (!this.als4.contains(coll)) {
-                                System.out.println("got: " + coll.toString());
-                                this.als4.add(coll);
-                            }
-                            else { System.out.println("got and jumped: " + coll.toString()); }
-
-                            System.out.println("numpoints: " + numPoints + "coll: " + coll.toString());
-                            // this.ls4List[this.numlines] = new LineSegment(p0, lineEnd);
-                            this.numlines++;
-                        }
-                        this.numPoints = 2;
-                    }
+            System.out.println("########## center point updated to " + centerPoint.toString() + " #########");
+            for (int i = 2; i < tempPoints.length; i++) {
+                if (Double.compare(centerPoint.slopeTo(tempPoints[i]), refSlope) == 0) {
+                    lineEnd = tempPoints[i];
+                    numCollPoints++;
+                    System.out.println("########## line segment sequence end updated to " + lineEnd.toString() + " #########");
                 }
 
-                System.out.println(this.als4.size());
-            }
+                else if (numCollPoints >= 4) {
+                    // set lineStart and lineEnd the right value.
+                    tempLineStartEnd[0] = centerPoint;
+                    tempLineStartEnd[1] = lineEnd;
+                    // after sorted, tempLineStartEnd[0] will be start of the line segment and tempLineStartEnd[1] will be end of the line segment
+                    Arrays.sort(tempLineStartEnd);
 
-            this.ls4List = new LineSegment[this.als4.size()];
-            for (int i = 0; i < this.als4.size(); i++) {
-                this.ls4List[i] = this.als4.get(i);
+                    // form and store the line segment
+                    this.als4.add(new LineSegment(lineStart, tempLineStartEnd[1]));
+                    System.out.println("########## line segment " + lineStart.toString() + " -> " + tempLineStartEnd[1].toString() + " created #########");
+
+                    // reset numCollPoints to 2
+                    numCollPoints = 2;
+
+                    // update refSlope to slope of center point and point i
+                    refSlope = centerPoint.slopeTo(tempPoints[i]);
+
+                    // reset lineStart and lineEnd -- to whom? encapsulate the code below into a method
+                    tempLineStartEnd[0] = centerPoint;
+                    tempLineStartEnd[1] = tempPoints[i];
+                    Arrays.sort(tempLineStartEnd);
+                    lineStart = tempLineStartEnd[0];
+                    lineEnd = tempLineStartEnd[1];
+                }
+
+                else {
+                    // reset numCollPoints to 2
+                    numCollPoints = 2;
+
+                    // update refSlope to slope of center point and point i (index of the next collinear sequence begin point)
+                    refSlope = centerPoint.slopeTo(tempPoints[i]);
+
+                    // reset lineStart and lineEnd -- to whom? encapsulate the code below into
+                    // a method named resetSequence(Point centerPoint, int sequenceStart)
+                    tempLineStartEnd[0] = centerPoint;
+                    tempLineStartEnd[1] = tempPoints[i];
+                    Arrays.sort(tempLineStartEnd);
+                    lineStart = tempLineStartEnd[0];
+                    lineEnd = tempLineStartEnd[1];
+                    System.out.println("########## line segment reseted to start from" + tempPoints[i].toString() + " #########");
+                }
+
+                // if there is only one line segment where all the points are collinear
+                // or if the final point is reached while the sequence containes long enough collinear points.
+                if (numCollPoints >= 4 && (i + 1) == points.length) {
+                    // set lineStart and lineEnd the right value.
+                    tempLineStartEnd[0] = centerPoint;
+                    tempLineStartEnd[1] = lineEnd;
+                    // after sorted, tempLineStartEnd[0] will be start of the line segment and tempLineStartEnd[1] will be end of the line segment
+                    Arrays.sort(tempLineStartEnd);
+
+                    // form and store the line segment
+                    this.als4.add(new LineSegment(lineStart, tempLineStartEnd[1]));
+                    System.out.println("########## single line segment created #########");
+                }
             }
         }
+
+        this.ls4List = new LineSegment[this.als4.size()];
+        for (int i = 0; i < this.als4.size(); i++) {
+            this.ls4List[i] = this.als4.get(i);
+        }
+
+        // if (points.length >= 4) {
+        //     for (Point p0 : points) {
+        //         Point[] tempPoints = points.clone();
+        //         Arrays.sort(tempPoints, p0.slopeOrder());
+        //         System.out.println("######### ######## ##########");
+        //         for (Point p : tempPoints) {
+        //             System.out.println("######### " + p.toString() + " ##########");
+        //         }
+        //         System.out.println("######### ######## ##########");
+        //
+        //         System.out.println("######### " + p0.toString() + " ##########");
+        //         Point lineStart = tempPoints[0];
+        //         Point lineEnd = tempPoints[1];
+        //         double refSlope = p0.slopeTo(lineEnd);
+        //         this.numPoints = 2;
+        //
+        //         for (int i = 1; i < tempPoints.length; i++) {
+        //             if (Double.compare(p0.slopeTo(tempPoints[i]), refSlope) == 0) {
+        //                 if (tempPoints[i].compareTo(lineEnd) > 0) {
+        //                     // set prevEnd point to start if its less than both current start and end points
+        //                     if (lineEnd.compareTo(lineStart) < 0) { lineStart = lineEnd; }
+        //
+        //                     // update current end point
+        //                     lineEnd = tempPoints[i];
+        //                 }
+        //
+        //                 if (tempPoints[i].compareTo(lineStart) < 0) {
+        //                     // set prevStart point to end if its greater than both current start and end points
+        //                     if (lineStart.compareTo(lineEnd) > 0) { lineEnd = lineStart; }
+        //
+        //                     // update current start point
+        //                     lineStart = tempPoints[i];
+        //                 }
+        //
+        //                 this.numPoints++;
+        //             }
+        //
+        //             else {
+        //                 refSlope = p0.slopeTo(tempPoints[i]);
+        //                 if (this.numPoints > 3) {
+        //                     LineSegment coll = new LineSegment(lineStart, lineEnd);
+        //                     if (!this.als4.contains(coll)) {
+        //                         System.out.println("got: " + coll.toString());
+        //                         this.als4.add(coll);
+        //                     }
+        //                     else { System.out.println("got and jumped: " + coll.toString()); }
+        //
+        //                     System.out.println("numpoints: " + numPoints + "coll: " + coll.toString());
+        //                     // this.ls4List[this.numlines] = new LineSegment(p0, lineEnd);
+        //                     this.numlines++;
+        //                 }
+        //                 this.numPoints = 2;
+        //             }
+        //         }
+        //
+        //         System.out.println(this.als4.size());
+        //     }
+        //
+        //     this.ls4List = new LineSegment[this.als4.size()];
+        //     for (int i = 0; i < this.als4.size(); i++) {
+        //         this.ls4List[i] = this.als4.get(i);
+        //     }
+        // }
 
         // for (Point p0 : points) {
         //     Arrays.sort(points, p0.slopeOrder());
